fce8addd9bbfc5ca9b20fd31cb35445d3b1b0529
Etny <ynteveeman@gmail.com>
2022-01-28 13:02:22 +0100

    Final commit (better day23-2)

--- day23-2/src/main.rs
+++ day23-2/src/main.rs
@@ -1,27 +1,27 @@
 mod input;
 
-
-use std::{fmt::Display, collections::BinaryHeap, time::Instant};
-use rustc_hash::FxHashMap;
+use std::{fmt::Display, collections::{BinaryHeap, HashMap}, time::Instant};
 use regex::Regex;
-use lazy_static::lazy_static;
 use input::*;
 
-const HEIGHT: u64 = 5;
-type PodMap = FxHashMap<(u64, u64), PodType>;
+const HEIGHT: u8 = 5;
+type PodType = u8;
+type PodMap = Vec<Option<PodType>>;
 
-lazy_static! {
-    static ref ROOM_INDICES: Vec<u64> = vec![2, 4, 6, 8]; 
-}
+static ROOM_INDICES: [u8; 4] = [2, 4, 6, 8];
 
+static POD_COST: [u32; 4] = [1, 10, 100, 1000];
+static POD_CHAR: [char; 4] = ['A', 'B', 'C', 'D'];
+
+
+// From 5 minute search time to 25 secs to now ~500 ms, not perfect but still happy with it.
 fn main() {
     let state = State::parse(INPUT.trim());
-
 
     println!("{}", solve(state));
 }
 
-fn solve(state: State) -> u64 {
+fn solve(state: State) -> u32 {
     let start = Instant::now();
     let mut heap = BinaryHeap::new();
     heap.push(state);
@@ -29,15 +29,11 @@ fn solve(state: State) -> u64 {
     while !heap.is_empty() {
         let state = heap.pop().unwrap();
 
-        // println!("{}", state);
-
         if state.cost_heuristic == 0 {
             println!("dT: {}", (Instant::now() - start).as_secs_f32());
-            println!("{}", state);
             return state.cost;
         }
 
-        // heap.extend(state.push_successors());
        state.push_successors(&mut heap);
     }
     
@@ -45,108 +41,123 @@ fn solve(state: State) -> u64 {
     0
 }
 
-    fn calc_moves(p1: (u64, u64), p2: (u64, u64)) -> u64 {
-        if p1.0 > p2.0 {
-            (p1.0 - p2.0) + p1.1 + p2.1
-        } else {
-            (p2.0 - p1.0) + p1.1 + p2.1
-        }
+fn calc_moves(p1: (u8, u8), p2: (u8, u8)) -> u8 {
+    if p1.0 > p2.0 {
+        (p1.0 - p2.0) + p1.1 + p2.1
+    } else {
+        (p2.0 - p1.0) + p1.1 + p2.1
     }
-
-
-struct State {
-    pods: PodMap,
-    to_move: Vec<(u64, u64)>,
-    cost: u64,
-    cost_heuristic: u64
-}   
-
-#[derive(PartialEq, Eq, Clone)]
-enum PodType {
-    A, B, C, D
 }
 
-impl State {
-    fn push_successors(&self, heap: &mut BinaryHeap<State>) /*-> Vec<Self>*/{
-        // let mut heap = vec![];
-        for pos in &self.to_move {
-            if self.pods[pos].is_home(*pos, &self.pods) { continue; }
+fn to_index((x, y): (u8, u8)) -> usize {
+    match y {
+        0 =>  x as usize,
+        _ => to_index_room((x, y))
+    }
+}
 
-            if let Some(home) = self.pods[pos].next_accessible_home(&self.pods) {
-                if self.reachable_from_x(pos.0).contains(&home.0) {
-                    heap.push(self.create_successor(*pos, home, true));
-                    // continue;
+//For when we know we are indexing a room, to avoid an unnecessary branching check
+fn to_index_room((x, y): (u8, u8)) -> usize {
+    (11 + ((x / 2 - 1) * 4) + (y - 1)) as usize
+}
+
+fn to_pos(index: usize) -> (u8, u8) {
+    match index {
+        0..=10 => (index as u8, 0),
+        _ => ((((index - 11) / 4 + 1) * 2) as u8, ((index - 11) % 4 + 1) as u8)
+    }
+}
+
+
+struct State{
+    pods: PodMap,
+    cost: u32,
+    cost_heuristic: u32
+}   
+
+
+impl State {
+    fn push_successors(&self, heap: &mut BinaryHeap<State>) {
+        'o: for pos_i in 0..self.pods.len()  {
+            if self.pods[pos_i].is_none() { continue; }
+
+            let pos = &to_pos(pos_i);
+
+            if is_home(self.pods[pos_i].unwrap(), *pos, &self.pods) { continue; }
+
+            if pos.1 > 1 {
+                for i in 1..pos.1 {
+                    if self.pods[pos_i - i as usize].is_some() { continue 'o; }
                 }
             }
+
+            if let Some(home) = next_accessible_home(self.pods[pos_i].unwrap(), &self.pods) {
+                if self.reachable_from_x(pos.0).contains(&home.0) {
+                    heap.push(self.create_successor(*pos, home, pos_i));
+                    continue;
+                }
+            }
+
             if pos.1 > 0 {
                 for x in self.reachable_from_x(pos.0) {
                     if ROOM_INDICES.contains(&x) { continue; }
-                    heap.push(self.create_successor(*pos, (x, 0), false));
+                    heap.push(self.create_successor(*pos, (x, 0), pos_i));
                 }
             }
          
         }   
-        // heap
     }
 
-    fn create_successor(&self, move_src: (u64, u64), move_dest: (u64, u64), moved_home: bool) -> Self {
-        let new_pods = self.pods.iter().map(|f| if *f.0 == move_src { (move_dest, f.1.clone()) } else { (*f.0, f.1.clone()) }).collect();
+    fn create_successor(&self, move_src: (u8, u8), move_dest: (u8, u8), index_src: usize) -> Self {
+        let pod = self.pods[index_src].unwrap();
+        
+        let mut new_pods = self.pods.clone();
+        new_pods.swap(to_index(move_dest), index_src);
 
-        let pod = &self.pods[&move_src];
-        let extra_cost = calc_moves(move_src, move_dest) * pod.move_cost();
+        let extra_cost = calc_moves(move_src, move_dest) as u32 * POD_COST[pod as usize];
 
-        let h = Self::heuristic_analysis(&new_pods);
+        let h = Self::heuristic_analysis(&new_pods);  
 
-        let mut to_move: Vec<_> = self.to_move.iter().filter(|r| **r != move_src).map(|r| *r).collect();
-
-        if moved_home {
-            to_move = to_move.into_iter().filter(|r| r.0 != move_dest.0).collect();
-        } else {
-            to_move.push(move_dest);
-        }
-
-        if move_src.1 != 0 && move_src.1 != HEIGHT-1 {
-            to_move.push((move_src.0, move_src.1 + 1));
-        }
-
-        Self { pods: new_pods, to_move, cost: self.cost + extra_cost, cost_heuristic: h }
+        Self { pods: new_pods, cost: self.cost + extra_cost, cost_heuristic: h }
     }
 
-    fn heuristic_analysis(pods: &PodMap) -> u64 {
+    fn heuristic_analysis(pods: &PodMap) -> u32 {
         let mut h = 0;
 
-        for pos in pods.keys() {
-            if pods[pos].is_home(*pos, &pods) { continue; }
-            let home = pods[pos].next_home(&pods); 
-            h += calc_moves(*pos, home) * pods[pos].move_cost();
+        for i in 0..pods.len() {
+            if let Some(pod) = pods[i]{
+                let pos = to_pos(i);
+                if is_home(pod, pos, &pods) { continue; }
+                let home = next_home(pod, &pods); 
+                h += calc_moves(pos, home) as u32 * POD_COST[pod as usize];
+            }
         }
 
         h
     }
 
-    fn reachable_from_x(&self, src_x: u64) -> Vec<u64> {
+    fn reachable_from_x(&self, src_x: u8) -> Vec<u8> {
         let mut out = vec![];
 
         if src_x > 0 { 
-            let mut i = src_x - 1;
+            let mut i = (src_x - 1) as usize;
             loop {
-                if self.pods.contains_key(&(i, 0)) { break; }
-                out.push(i);
+                if self.pods[i].is_some() { break; }
+                out.push(i as u8);
 
                 if i == 0 { break; } else { i -= 1; }
             }
         }
 
         if src_x < 10 {
-            let mut i = src_x + 1;
+            let mut i = (src_x + 1) as usize;
             loop {
-                if self.pods.contains_key(&(i, 0)) { break; }
-                out.push(i);
+                if self.pods[i].is_some() { break; }
+                out.push(i as u8);
 
                 if i >= 10 { break; } else { i += 1; }
             }
         }
-
 
         out
     }
@@ -162,25 +173,24 @@ impl State {
                 captures
                     .iter()
                     .skip(1)
-                    .map(|d| PodType::from_dec(d.unwrap().as_str())),
+                    .map(|d| from_dec(d.unwrap().as_str())),
             )
-            .collect::<FxHashMap<_, _>>();
-        let to_move: Vec<_> = ROOM_INDICES.iter().map(move |r| (*r, 1)).collect();
+            .collect::<HashMap<_, _>>();
+        let pods = (0..27).map(|p| pods.get(&to_pos(p)).map_or(None, move |f| Some(*f))).collect::<Vec<_>>();
 
-        Self { pods, to_move, cost: 0, cost_heuristic: 1 }
+        Self { pods, cost: 0, cost_heuristic: 1 }
     }
 }
 
-impl PodType {
 
-    fn is_home(&self, (x, y): (u64, u64), pods: &PodMap) -> bool {
-        if x != self.dest_x() || y == 0 {
+    fn is_home(pod: PodType, (x, y): (u8, u8), pods: &PodMap) -> bool {
+        if x != ROOM_INDICES[pod as usize] || y == 0 {
             false
         } else if y == HEIGHT-1 {
             true
         } else {
             for i in y+1..HEIGHT {
-                if pods.get(&(x, i)).map_or(false, |f| *f != *self) {
+                if pods[to_index_room((x, i))].map_or(false, |f| f != pod) {
                     return false;
                 }
             }
@@ -189,12 +199,12 @@ impl PodType {
         }
     }
 
-    fn next_accessible_home(&self, pods: &PodMap) -> Option<(u64, u64)> {
-        let x = self.dest_x();
+    fn next_accessible_home(pod: PodType, pods: &PodMap) -> Option<(u8, u8)> {
+        let x = ROOM_INDICES[pod as usize];
         for y in (1..HEIGHT).rev() {
-            match pods.get(&(x, y)) {
+            match pods[to_index_room((x, y))] {
                 None => return Some((x, y)),
-                Some(p) if *p == *self => continue,
+                Some(p) if p == pod => continue,
                 Some(_) => return None
             }
         }
@@ -202,66 +212,37 @@ impl PodType {
         None
     }
 
-    fn next_home(&self, pods: &PodMap) -> (u64, u64) {
-        let x = self.dest_x();
+    fn next_home(pod: PodType, pods: &PodMap) -> (u8, u8) {
+        let x = ROOM_INDICES[pod as usize];
         for y in (1..HEIGHT).rev() {
-            match pods.get(&(x, y)) {
-                Some(p) if *p == *self => continue,
+            match pods[to_index_room((x, y))] {
+                Some(p) if p == pod => continue,
                 _ => return (x, y)
             }
         }
 
-        (0, 0)
+        panic!()
     }
 
-    fn from_dec(dec: &str) -> Self {
+    fn from_dec(dec: &str) -> PodType {
         match dec {
-            "A" => Self::A,
-            "B" => Self::B,
-            "C" => Self::C,
-            "D" => Self::D,
+            "A" => 0,
+            "B" => 1,
+            "C" => 2,
+            "D" => 3,
             _ => panic!(),
         }
     }
 
-    fn move_cost(&self) -> u64 {
-        match *self {
-            Self::A => 1,
-            Self::B => 10,
-            Self::C => 100,
-            Self::D => 1000
-        }
-    }
-
-    fn dest_x(&self) -> u64 {
-        match *self {
-            Self::A => 2,
-            Self::B => 4,
-            Self::C => 6,
-            Self::D => 8 
-        }
-    }
-
-    pub fn to_char(&self) -> char {
-        match *self {
-            Self::A => 'A',
-            Self::B => 'B',
-            Self::C => 'C',
-            Self::D => 'D'
-        }
-    }
-    
-}
-
-impl PartialEq for State {
+impl  PartialEq for State  {
     fn eq(&self, other: &Self) -> bool {
         (self.cost + self.cost_heuristic) == (other.cost + other.cost_heuristic)
     }
 }
 
-impl Eq for State {}
+impl  Eq for State  {}
 
-impl PartialOrd for State {
+impl  PartialOrd for State  {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
         Some((other.cost + other.cost_heuristic).cmp(&(self.cost + self.cost_heuristic)))
     }
@@ -277,7 +258,6 @@ impl Display for State {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         writeln!(f)?;
 
-        writeln!(f, "to_move: {:?}", self.to_move)?;
         writeln!(f, "cost: {}, h: {}", self.cost, self.cost_heuristic)?;
 
         for y in 0..HEIGHT {
@@ -285,7 +265,7 @@ impl Display for State {
                 if y != 0 && !ROOM_INDICES.contains(&i) {
                     write!(f, " ")?;
                 } else {
-                    write!(f, "{}", self.pods.get(&(i, y)).map_or('.', |f| f.to_char()))?;
+                    write!(f, "{}", self.pods[to_index((i, y))].map_or('.', |f| POD_CHAR[f as usize]))?;
                 }
             }
             writeln!(f)?;
